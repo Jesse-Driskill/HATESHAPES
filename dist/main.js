ab9610f0feb41e07f0fb975d86e2297bc9d4e6a5
Jesse-Driskill <jesse@driskill.com>
2022-04-25 19:34:59 -0700

    Adds Files

---
+++ dist/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("\r\n\r\ndocument.addEventListener(\"DOMContentLoaded\", () => {\r\n    let p = document.createElement(\"h1\");\r\n    let o = document.getElementById(\"sup\");\r\n    p.innerHTML = \"hehehjehehe\";\r\n    o.appendChild(p);\r\n    const canvas = document.getElementById(\"canvas\");\r\n    \r\n});\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    let p = document.createElement("h1");
+    let o = document.getElementById("sup");
+    p.innerHTML = "hehehjehehe";
+    o.appendChild(p);
+    const canvas = document.getElementById("canvas");
+    
+});
