305e19e29ecd957487cee1a940afe43ee8e1ae03
Jesse-Driskill <jesse@driskill.com>
2022-04-25 20:38:33 -0700

    Removes node_modules

--- src/Shape.js
+++ src/Shape.js
@@ -1,4 +1,4 @@
-export default class Shape {
+export class Shape {
     constructor(pos) {
         
     }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
+import { Game } from "./Game";
+import { Shape } from "./Shape";
 
+window.Game = new Game();
+window.Shape = new Shape();
 
 document.addEventListener("DOMContentLoaded", () => {
-    let p = document.createElement("h1");
-    let o = document.getElementById("sup");
-    p.innerHTML = "hehehjehehe";
-    o.appendChild(p);
-    const canvas = document.getElementById("canvas");
     
+    let canvas = document.getElementById("canvas");
+    let ctx = canvas.getContext('2d');
+
+    
+    ctx.beginPath();
+    
+    
+    ctx.arc(69, 69, 69, 0, 2*Math.PI, false);
+    ctx.fillStyle = "FFFFFF";
+    ctx.fill();
 });
